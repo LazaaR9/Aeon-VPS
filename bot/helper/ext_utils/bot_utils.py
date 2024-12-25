46c969c8af1f92a2a8f3ec1653018ccc65e15145
LazaaR <Githeroku@outlook.com>
2024-12-25 22:39:38 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -321,8 +321,8 @@ def get_readable_message():
     msg += f"<u><b>TASKS</b>: {tasks}{bmax_task}</u>"
     msg += f"\n<b>BotUP</b>: {current_time} | "
     msg += f"<b>Free</b>: {get_readable_file_size(disk_usage('/usr/src/app/downloads/').free)}"
-    msg += f"\n<b>UL</b>: {get_readable_file_size(up_speed)}/s | "
-    msg += f"<b>DL</b>: {get_readable_file_size(dl_speed)}/s"
+    msg += f"\n<b>DL</b>: {get_readable_file_size(dl_speed)}/s | "
+    msg += f"<b>UL</b>: {get_readable_file_size(up_speed)}/s"
     return msg, button
 
 
